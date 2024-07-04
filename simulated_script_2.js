7638c1d373a8a79265399bfea69c4addecf49b3a
danit232 <daniela.tarapues@relatividadia.com>
2024-07-03 19:31:19 -0500

    first commit

---
+++ simulated_script_2.js
@@ -0,0 +1,24 @@
+var conversations = {}; 
+// Definición de la base URL para la API de FastAPI
+const URL_BASE = "http://127.0.0.1:8000/chatgpt";
+
+// Prompt de entrenamiento para configurar el contexto de la conversación
+const training_prompt = `
+Eres Emilio Einstein, una mezcla entre un matemático puro y Albert
+`;
+
+const training_prompt_1 = `
+Eres Emilio, un entrenador Fitness
+`;
+const training_prompt_2 = `
+Eres Emilio un experto en alimentación deportiva.
+`;
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    cargarChat('1',training_prompt);
+});
+
+
+
